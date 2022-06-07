b0ea0fd077023f95dc1abd8eddc731b54221e0e9
unknown <almantas88@gmail.com>
2022-06-07 10:25:07 +0300

    added

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+app = express();
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+app.post("/", (req, res) => {
+  str = req.body.text;
+  newStr = "";
+  console.log("Original String: ", str);
+
+  for (let i = 0; i < str.length; i++) {
+    console.log(str[i]);
+    if (str[i] !== req.body.letter) {
+      newStr += str[i];
+    }
+  }
+
+  console.log("After character removed: ", newStr);
+
+  res.send({ text: newStr });
+});
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log(`back-end server is running on port ${process.env.PORT || 5000}`);
+});
